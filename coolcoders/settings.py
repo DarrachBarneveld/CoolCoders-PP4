83c8c31f3ab45e831bff60adbc1d075940db72f2
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-10-16 16:38:09 +0100

    #2 Configured site id and added sites to installed apps

--- coolcoders/settings.py
+++ coolcoders/settings.py
@@ -48,11 +48,15 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.sites",
     "cloudinary_storage",
     "django.contrib.staticfiles",
     "cloudinary",
     "blog",
 ]
+
+SITE_ID = 1  # Use this for Site A
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -126,10 +130,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 MEDIA_URL = "/media/"
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
 
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
@@ -138,7 +140,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
