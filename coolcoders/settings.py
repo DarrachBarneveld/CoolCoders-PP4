daa08ee3b6280cd86abfa75511eb44a2cd415064
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-10-16 16:43:17 +0100

    Testing deployment with Debug set to false

--- coolcoders/settings.py
+++ coolcoders/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "https://coolcoders-b69260c4617a.herokuapp.com/",
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     "blog",
 ]
 
-SITE_ID = 1  # Use this for Site A
+SITE_ID = 1
 
 
 MIDDLEWARE = [
