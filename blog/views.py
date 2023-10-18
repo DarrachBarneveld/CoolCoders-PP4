363b390c2385168bc0ff7595be14db9e8b56b91a
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-10-18 05:50:19 +0100

    Created homepage view and url pattern

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
+"""Views"""
+
+# pylint: disable=E1101
+
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Article
+
 
 # Create your views here.
+class HomePageView(View):
+    """
+    A view class for displaying a list of categories and featured posts on the homepage.
+
+    """
+
+    def get(self, request, *args, **kwargs):
+        """
+        Retrieves the posts from the database and returns the most popular, trending by likes
+        and editors pick
+
+        """
+        all_articles = Article.objects.filter(approved=True)
+
+        return render(
+            request,
+            "home.html",
+            {
+                "all_articles": all_articles,
+            },
+        )
