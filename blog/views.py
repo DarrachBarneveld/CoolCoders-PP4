c9ffb2cac3202cc8dc22b06957c759465cd8a762
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-10-18 14:11:21 +0100

    #16 Updated HomepageView to render filtered articles

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 """Views"""
 
 # pylint: disable=E1101
-from django.shortcuts import render
-from django.views import View
-from .models import Post
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Count
+from django.views.generic import View
+from .models import Post, Comment
 
 
-# Create your views here.
 class HomePageView(View):
     """
     A view class for displaying a list of categories and featured posts on the homepage.
@@ -20,12 +20,35 @@ class HomePageView(View):
 
         """
         all_posts = Post.objects.filter(approved=True)
-        print(all_posts)
+        popular_post = (
+            all_posts.annotate(comment_count=Count("comments"))
+            .order_by("-comment_count")
+            .first()
+        )
+
+        popular_post.comment_count = Comment.objects.filter(
+            post=popular_post, approved=True
+        ).count()
+
+        trending_posts = all_posts.annotate(like_count=Count("likes")).order_by(
+            "-like_count"
+        )[:3]
+
+        editors_pick = get_object_or_404(
+            Post, slug="ais-influence-on-tech-shaping-the-future"
+        )
+
+        editors_pick.comment_count = Comment.objects.filter(
+            post=editors_pick, approved=True
+        ).count()
 
         return render(
             request,
             "home.html",
             {
                 "all_posts": all_posts,
+                "popular_post": popular_post,
+                "trending_posts": trending_posts,
+                "editors_pick": editors_pick,
             },
         )
