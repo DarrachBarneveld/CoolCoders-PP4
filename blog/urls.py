d792ccc8f8624d12a40c2457acd2be89930b3fde
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-10-19 14:00:18 +0100

    #12 Linked Addpostpage view to urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path("<slug:slug>/", views.CategoryPage.as_view(), name="index"),
+    path("add_post/", views.AddPostPage.as_view(), name="add_post"),
     path("post/<slug:slug>/", views.PostDetailPage.as_view(), name="post-detail"),
 ]
